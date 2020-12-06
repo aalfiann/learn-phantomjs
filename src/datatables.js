5c24bf104ed4236106b56556c4c0c37ea3099626
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2020-12-06 10:08:17 +0700

    - update

--- src/datatables.js
+++ src/datatables.js
@@ -30,6 +30,9 @@ if (system.args.length !== 1) {
   console.log('invalid call');
   phantom.exit(1);
 } else {
+  // Set another User Agent
+  page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
+
   // set viewport
   page.viewportSize = {
     width: 800,
